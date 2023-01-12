988d6c59429b23c05433b316a012417aa9c24bcf
Thibault DE-PERMENTIER <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-01-12 14:32:37 +0100

    fixing error with file name

--- Controleurs/ControleurRecipe.php
+++ Controleurs/ControleurRecipe.php
@@ -6,6 +6,7 @@ final class ControleurRecipe
     {
         $O_recipe =  new Recipe();
         Vue::montrer('recipe/mainView', array('recipe' =>  $O_recipe->donneMessage()));
+        Vue::montrer('recipe/list', array('recipe' =>  $O_recipe->donneMessage()));
 
     }
 
