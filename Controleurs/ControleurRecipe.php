e929098016f7263f8340f247d10a16a5a7e2a8f6
Thibault DE-PERMENTIER <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-01-12 16:03:26 +0100

    more working view for recipe, again

--- Controleurs/ControleurRecipe.php
+++ Controleurs/ControleurRecipe.php
@@ -5,9 +5,11 @@ final class ControleurRecipe
     public function defautAction()
     {
         $O_recipe =  new Recipe();
-        Vue::montrer('recipe/mainView', array('recipe' => $O_recipe->donneMessage()));
+        Vue::montrer('recipe/mainInfoView', array('recipe' => $O_recipe->donneMessage()));
         Vue::montrer('recipe/listView', array('recipe' => $O_recipe->donneMessage()));
         Vue::montrer('recipe/listView', array('recipe' => $O_recipe->donneMessage()));
+        Vue::montrer('recipe/imageView', array('recipe' => $O_recipe->donneMessage2()));
+        Vue::montrer('recipe/textView', array('recipe' => $O_recipe->donnemessage()));
         Vue::montrer('recipe/imageView', array('recipe' => $O_recipe->donneMessage2()));
     }
 
---
+++ Vues/recipe/textView.php
@@ -0,0 +1,3 @@
+<?php
+echo '<p>'.$A_vue['recipe'][0].'</p>';
+echo '<p>'.$A_vue['recipe'][1].'</p>';
