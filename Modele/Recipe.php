b3bf4e1a78f339d7c74a961e21d1e607cab930ff
Romain CLEMENT <rc2301@orange.fr>
2023-01-16 17:39:21 +0100

    added Recipe ustensils array

--- Modele/Recipe.php
+++ Modele/Recipe.php
@@ -2,15 +2,24 @@
 
 final class Recipe{
 
-    private static $sql = 'SELECT NAME,DESCRIPTION,DURATION,ID_AUTHOR,ID_DIFFICULTY,ID_COST FROM RECIPE WHERE ID_RECIPE=?';
-    private static $req_prep;
+    private static $sql1 = 'SELECT NAME,DESCRIPTION,DURATION,ID_AUTHOR,ID_DIFFICULTY,ID_COST FROM RECIPE WHERE ID_RECIPE=?';
+    private static $sql2 = 'SELECT ID_USTENSIL FROM RECIPE_USTENSIL WHERE ID_RECIPE=?';
 
-    public static function getById($id){
-        if (is_null(self::$req_prep))
-            self::$req_prep = modele::$pdo->prepare(self::$sql);
-        self::$req_prep->execute(array($id));
-        $row = self::$req_prep->fetch();
-        return new Recipe($id, $row['NAME'],$row ['DESCRIPTION'], $row['DURATION'], User::getById($row['ID_AUTHOR']), Difficulty::getById($row['ID_DIFFICULTY']), Cost::getById($row['ID_COST']));
+    private static $req_prep1;
+    private static $req_prep2;
+
+    public static function getById($id) {
+        if (is_null(self::$req_prep1))
+            self::$req_prep1 = modele::$pdo->prepare(self::$sql1);
+        if (is_null(self::$req_prep2))
+            self::$req_prep2 = modele::$pdo->prepare(self::$sql2);
+        self::$req_prep1->execute(array($id));
+        $row = self::$req_prep1->fetch();
+        self::$req_prep2->execute(array($id));
+        $ustensils = array();
+        while ($idUstensil = self::$req_prep2->fetch())
+            array_push($ustensils, Ustensil::getById($idUstensil['ID_USTENSIL']));
+        return new Recipe($id, $row['NAME'],$row ['DESCRIPTION'], $row['DURATION'], User::getById($row['ID_AUTHOR']), Difficulty::getById($row['ID_DIFFICULTY']), Cost::getById($row['ID_COST']), $ustensils);
     }
 
     private $id;
@@ -20,8 +29,9 @@ final class Recipe{
     private $author;
     private $difficulty;
     private $cost;
+    private $ustensils;
 
-    private function __construct($id, $name, $description, $duration, $author, $difficulty, $cost){
+    private function __construct($id, $name, $description, $duration, $author, $difficulty, $cost, $ustensils){
         $this-> id = $id;
         $this->name = $name;
         $this-> description= $description;
@@ -29,6 +39,7 @@ final class Recipe{
         $this->author = $author;
         $this->difficulty = $difficulty;
         $this->cost = $cost;
+        $this->ustensils = $ustensils;
     }
     
     public function getId(){
@@ -59,8 +70,12 @@ final class Recipe{
         return $this->cost;              
     }
 
+    public function getUstencils(){
+        return $this->ustensils;             
+    }
+
     public function __toString() {
-        return "Recipe{id=" . $this->getId() . ", name=" . $this->getName() . ", description=" . $this->getDescription() . ", duration=" . $this->getDuration() . ", author=" . $this->getAuthor() . ", difficulty=" . $this->getDifficulty() . ", cost=" . $this->getCost() . "}";
+        return "Recipe{id=" . $this->getId() . ", name=" . $this->getName() . ", description=" . $this->getDescription() . ", duration=" . $this->getDuration() . ", author=" . $this->getAuthor() . ", difficulty=" . $this->getDifficulty() . ", cost=" . $this->getCost() . ", ustensils=" . implode($this->getUstencils()) . "}";
     }
 
 }
