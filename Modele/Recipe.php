e1f3c51c9b9bf9123ecc1ddb4d889a129b22ccb1
Romain CLEMENT <rc2301@orange.fr>
2023-01-16 17:04:43 +0100

    added User  Recipe

--- Modele/Recipe.php
+++ Modele/Recipe.php
@@ -1,41 +1,66 @@
 <?php
 
-final class Recipe
-{
-    private $_S_message = 'ceci est le titre de la recette';
-    private $_S_message1 = ['ceci est une dififculté','ceci est un temps','et ceci est une note'];
-    private $_S_message2 = ["ceci est l'ingrédient 1","ceci est l'ingrédient 2","ceci est l'ingrédient 3"];
-    private $_S_message3 = ["ceci est l'ustensile 1","ceci est l'ustensile 2","ceci est l'ustensile 3"];
-    private $_S_message4 = ['image/galette_des_rois.jpeg','ceci est une galette des roi'];
-    private $_S_message5 = ['ceci est le texte qui explique comment faire la recette','ceci est le coût de la recette'];
+final class Recipe{
 
-    public function donneMessage()
-    {
-        return $this->_S_message ;
+    private static $sql = 'SELECT NAME,DESCRIPTION,DURATION,ID_AUTHOR,ID_DIFFICULTY,ID_COST FROM RECIPE WHERE ID_RECIPE=?';
+    private static $req_prep;
+
+    public static function getById($id){
+        if (is_null(self::$req_prep))
+            self::$req_prep = modele::$pdo->prepare(self::$sql);
+        self::$req_prep->execute(array($id));
+        $row = self::$req_prep->fetch();
+        return new Recipe($id, $row['NAME'],$row ['DESCRIPTION'], $row['DURATION'], User::getById($row['ID_AUTHOR']), Difficulty::getById($row['ID_DIFFICULTY']), Cost::getById($row['ID_COST']));
     }
 
-    public function donneMessage1()
-    {
-        return $this->_S_message1 ;
+    private $id;
+    private $name;
+    private $description;
+    private $duration;
+    private $author;
+    private $difficulty;
+    private $cost;
+
+    private function __construct($id, $name, $description, $duration, $author, $difficulty, $cost){
+        $this-> id = $id;
+        $this->name = $name;
+        $this-> description= $description;
+        $this-> duration= $duration;
+        $this->author = $author;
+        $this->difficulty = $difficulty;
+        $this->cost = $cost;
+    }
+    
+    public function getId(){
+        return $this->id;              
     }
 
-    public function donneMessage2()
-    {
-        return $this->_S_message2 ;
+    public function getName(){
+        return $this->name;              
     }
 
-    public function donneMessage3()
-    {
-        return $this->_S_message3 ;
+    public function getDescription(){
+        return $this->description;
     }
 
-    public function donneMessage4()
-    {
-        return $this->_S_message4 ;
+    public function getDuration(){
+        return $this->duration;
     }
 
-    public function donneMessage5()
-    {
-        return $this->_S_message5 ;
+    public function getAuthor(){
+        return $this->author;              
     }
+
+    public function getDifficulty(){
+        return $this->difficulty;              
+    }
+
+    public function getCost(){
+        return $this->cost;              
+    }
+
+    public function __toString() {
+        return "Recipe{id=" . $this->getId() . ", name=" . $this->getName() . ", description=" . $this->getDescription() . ", duration=" . $this->getDuration() . ", author=" . $this->getAuthor() . ", difficulty=" . $this->getDifficulty() . ", cost=" . $this->getCost() . "}";
+    }
+
 }
