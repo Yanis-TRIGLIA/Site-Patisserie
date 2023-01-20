ee0e03382a16922590cb4d5a8771c638d71477b1
Thibault-De-Permentier <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2023-01-20 09:15:08 +0100

    fixing things related to list's views

--- Vues/recipe/listUstensileView.php
+++ Vues/recipe/listUstensileView.php
@@ -3,7 +3,7 @@ echo '<div classe="Ustensiles">';
 echo '<p><strong>Ustensiles :</strong></p>';
 echo '<ul class="la_liste_ustensiles">';
 for($i = 0; $i < count($A_vue['recipe']); ++$i){
-    echo '<li>'.$A_vue['recipe'][$i].'</li>'."\n";
+    echo '<li>'.$A_vue['recipe'][$i]->getName().'</li>'."\n";
 }
 echo '</ul>';
 echo '</div>';
