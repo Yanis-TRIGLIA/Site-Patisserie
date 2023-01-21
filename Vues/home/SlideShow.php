34055816ea7425e4e7e8dc8d4f72eaa0868691e5
Yanis TRIGLIA <91632872+Yanis-TRIGLIA@users.noreply.github.com>
2023-01-21 18:33:38 +0100

    Update SlideShow with new div

--- Vues/home/SlideShow.php
+++ Vues/home/SlideShow.php
@@ -1,30 +1,35 @@
 <?php
-echo '<link rel="stylesheet" href="/Css/styleaccueil.css">';
-echo '<div class= "recette_random">';
-echo '<div class= "recette_1">';
+error_reporting(0);
+echo '<link rel="stylesheet" href="Css/stylesite.css">';
+echo'<div id="imagePrincipale">';
+echo'<h1>IUT Gourmand</h1>';
+echo'<div id="premierTrait"></div>';
+echo'<h3>Le Site de Patisserie Officiel du Département Informatique</h3>';
+echo'</div>';
+
+echo '<div class= "Random_recipe">';
+echo '<div class= "recipe_1">';
 echo  "<a href='recipe/edit/" . $A_vue[0][5] . "'>";
-echo '<img class="galette_des_rois" src='.$A_vue[0][0].' alt="'.$A_vue[1].'"/>';
+echo '<img class="Picture_recipe_1" src='.$A_vue[0][0].' alt="'.$A_vue[1].'"/>';
 echo "<p class=small-text>" ."Nom :". $A_vue[0][1] . "</p>";
 echo "<p class=small-text>" . "Difficulter :".$A_vue[0][2] . "</p>";
 echo "<p class=small-text>" ."Durée :". $A_vue[0][3]. "\n"."Min"."</p>";
 echo '</a>';
 echo '</div>';
-echo '<div class= "recette_2">';
+echo '<div class= "recipe_2">';
 echo  "<a href='recipe/edit/" . $A_vue[1][5] . "'>";
-echo '<img class="galette_des_rois" src='.$A_vue[1][0].' alt="'.$A_vue[1].'"/>';
+echo '<img class="Picture_recipe_2" src='.$A_vue[1][0].' alt="'.$A_vue[1].'"/>';
 echo "<p class=small-text>" ."Nom :". $A_vue[1][1] . "</p>";
 echo "<p class=small-text>" . "Difficulter :".$A_vue[1][2] . "</p>";
 echo "<p class=small-text>" ."Durée :". $A_vue[1][3]. "\n"."Min"."</p>";
 echo '</a>';
 echo '</div>';
-echo '<div class= "recette_3">';
+echo '<div class= "recipe_3">';
 echo  "<a href='recipe/edit/" . $A_vue[2][5] . "'>";
-echo '<img class="galette_des_rois" src='.$A_vue[2][0].' alt="'.$A_vue[1].'"/>';
+echo '<img class="Picture_recipe_3" src='.$A_vue[2][0].' alt="'.$A_vue[1].'"/>';
 echo "<p class=small-text>" ."Nom :". $A_vue[2][1] . "</p>";
 echo "<p class=small-text>" . "Difficulter :".$A_vue[2][2] . "</p>";
 echo "<p class=small-text>" ."Durée :". $A_vue[2][3]. "\n"."Min"."</p>";
 echo '</a>';
 echo '</div>';
 echo '</div>';
-
-  //echo "<a href='recipe/edit/" . $cake['ID_RECIPE'] . "'>";
