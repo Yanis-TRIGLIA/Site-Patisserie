371121e0a6dab3180752bfdb75c5cbbef86ed676
Yanis TRIGLIA <91632872+Yanis-TRIGLIA@users.noreply.github.com>
2023-01-23 23:00:47 +0100

    Update for filter

--- Vues/home/SearchResults.php
+++ Vues/home/SearchResults.php
@@ -1,5 +1,33 @@
 <?php
- echo '<link rel="stylesheet" href="/Css/stylesite.css">';
+echo '<link rel="stylesheet" href="/Css/stylesite.css">';
+
+echo '<button id="filter-button" onclick="showFilters()">Filtres</button>';
+echo '<div id="filter-options" style="display: none;">';
+echo '<form method="post" action="/home/filter">';
+echo '<input type="text" name="name" placeholder="Rechercher une recette">';
+echo '<label for="difficulty">Difficulté :</label>';
+echo '<select name="difficulty" id="difficulty">';
+echo '<option value=Facile>Facile</option>';
+echo '<option value=Moyen>Moyen</option>';
+echo '<option value=Difficile>Difficile</option>';
+echo '</select>';
+echo '<label for="duration">Durée :</label>';
+echo '<input type="text" name="duration" id="duration" placeholder="en minutes">';
+echo '<input type="submit" value="Filtrer">';
+echo '</form>';
+echo '</div>';
+ ?>
+ <script>
+   function showFilters() {
+     var filterOptions = document.getElementById("filter-options");
+     if (filterOptions.style.display === "none") {
+       filterOptions.style.display = "block";
+     } else {
+       filterOptions.style.display = "none";
+     }
+   }
+ </script>
+ <?php
 echo '<div class= "Search_Results">';
 if($A_vue == null){
     echo '<h2 class="moment_title">Aucun résultat</h2>';
