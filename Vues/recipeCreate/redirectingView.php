0a9dff87702b24c247cc9935c22d8964da50441e
imuny <92868641+imuny0@users.noreply.github.com>
2023-01-26 18:53:29 +0100

    add css redirectingView

---
+++ Vues/recipeCreate/redirectingView.php
@@ -0,0 +1,5 @@
+<?php
+echo '<body class="redirecting_view_body">';
+echo '<a  href="localhost/home"><button class="redirecting_view_home">Home</button></a><br>';
+echo '<a href="defaut/'.$A_vue.'"><button class="redirecting_view_recipe">Votre recette</button></a><br>';
+echo '</body>';
