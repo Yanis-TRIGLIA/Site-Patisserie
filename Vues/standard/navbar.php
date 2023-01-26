d6d0d2a81a080961db2baa0ce437407fd4c98412
Yanis TRIGLIA <91632872+Yanis-TRIGLIA@users.noreply.github.com>
2023-01-26 13:35:34 +0100

    Update navbar for Connexion

--- Vues/standard/navbar.php
+++ Vues/standard/navbar.php
@@ -1,16 +1,50 @@
-<?php 
-echo '<div class="Navigation_bar">';
-echo '<form method="post" action="/home/search" class="Navigation_bar">';
-echo '<ul>';
-echo '<li class><a class="backgroundimage" href="https://tpphp1.alwaysdata.net/home">_ </a></li>';
-echo '<li style="float:middle">';
-echo '<input class="Search" type="text" name="query" placeholder="Recherche...">';
-echo '</li>';
-echo '<li style="float:middle">';
-echo '<button class="Searchbutton" type="submit"></button>';
-echo '</li>';
-echo '<li style="float:right"><a class="login" href="/home/logIn">se connecter</a></li>';
-echo '<li style="float:right"><a class="sigin" href="/home/register">s\'enregistrer</a></li>';
-echo '</ul>';
-echo '</form>';
-echo '</div>';
+<?php
+if (isset($_SESSION['user'])) {
+    // L'utilisateur est connecté
+    echo
+    '<div class="Navigation_bar">
+    <link rel="stylesheet" href="/Css/stylesite.css">
+    <form method="post" action="/home/search" class="Navigation_bar">
+    <ul>
+    <li class><a class="backgroundimage" href="https://tpphp1.alwaysdata.net">_ </a></li>
+    <li style="float:middle">
+    <input class="Search" type="text" name="query" placeholder="Recherche...">
+    </li>
+    <li style="float:middle">
+    <button class="Searchbutton" type="submit"></button>
+    </li>
+
+    <li style="float:right">
+    <a href="/home" class="link_profile" style="padding-top: 8px">
+    <img class="logout_button" src="https://cdn-icons-png.flaticon.com/512/126/126467.png" width="50" height="50" ">
+    </a>
+    </li>
+    
+    <li style="float:right">
+    <a href="/home/profile" class="link_profile" style="padding-top: 8px">
+    <img class="profil_button" src="https://cdn-icons-png.flaticon.com/512/25/25634.png" width="50" height="50" ">
+    </a>
+    </li>
+    
+    
+    </ul>
+    </form>
+    </div>';
+} else {
+    // L'utilisateur n'est pas connecté
+    echo '<div class="Navigation_bar">';
+    echo '<form method="post" action="/home/search" class="Navigation_bar">';
+    echo '<ul>';
+    echo '<li class><a class="backgroundimage" href="https://tpphp1.alwaysdata.net/home">_ </a></li>';
+    echo '<li style="float:middle">';
+    echo '<input class="Search" type="text" name="query" placeholder="Recherche...">';
+    echo '</li>';
+    echo '<li style="float:middle">';
+    echo '<button class="Searchbutton" type="submit"></button>';
+    echo '</li>';
+    echo '<li style="float:right"><a class="login" href="/home/logIn">se connecter</a></li>';
+    echo '<li style="float:right"><a class="sigin" href="/home/register">s\'enregistrer</a></li>';
+    echo '</ul>';
+    echo '</form>';
+    echo '</div>';
+}
